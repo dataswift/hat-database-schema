35561b0be5f4053c1f47eb437a563e036e612d72
andrius <andrius@cambridgecoding.com>
2016-05-31 14:04:59 +0100

    envionment variables used by default HAT configuration

--- env.sh
+++ env.sh
@@ -4,3 +4,9 @@ export DATABASE=${DATABASE:-"hat20"}
 export DBUSER=${DBUSER:-$DATABASE}
 export DBPASS=${DBPASS:-"pa55w0rd"}
 export JDBCURL="jdbc:postgresql://localhost/$DATABASE"
+
+export POSTGRES_DB=$DATABASE
+export POSTGRES_USER=$DBUSER
+export POSTGRES_PASSWORD=$DBPASS
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
