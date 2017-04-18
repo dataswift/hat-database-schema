c47555bb9522e5adb55c278270af0737696dbc39
andrius <andrius.aucinas@gmail.com>
2017-04-18 18:38:20 +0100

    adds a notion of grouped data records

--- 14_newHat.sql
+++ 14_newHat.sql
@@ -12,3 +12,21 @@ CREATE TABLE hat.data_json (
 );
 
 --rollback DROP TABLE hat.data_json;
+
+--changeset hubofallthings:jsonDataGroups context:structuresonly
+
+CREATE TABLE hat.data_json_groups (
+  group_id UUID      NOT NULL PRIMARY KEY,
+  owner    UUID      NOT NULL REFERENCES hat.user_user (user_id),
+  date     TIMESTAMP NOT NULL DEFAULT (NOW())
+);
+
+CREATE TABLE hat.data_json_group_records (
+  group_id  UUID NOT NULL REFERENCES hat.data_json_groups (group_id),
+  record_id UUID NOT NULL REFERENCES hat.data_json (record_id),
+  PRIMARY KEY (group_id, record_id)
+);
+
+--rollback DROP TABLE hat.data_json_group_records;
+--rollback DROP TABLE hat.data_json_groups;
+
