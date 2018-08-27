c0706381f5de41b3063061d05267078498f83b54
whiteshadow-gr <marios4th@yahoo.gr>
2018-08-27 19:05:06 +0100

    changed sentiment tool logo and screenshots

--- 14_newHat.sql
+++ 14_newHat.sql
@@ -1081,29 +1081,7 @@ VALUES ('data-feed-counter', true, null, null);
 --changeset hubofallthings:changedSentimentFunctionIcon context:structuresonly
 
 UPDATE hat.she_function
-    SET info.graphics = '
-    {
-      "banner": {
-          "normal": ""
-      },
-      "logo": {
-          "normal": "https://github.com/Hub-of-all-Things/exchange-assets/blob/master/Sentiments/Image%20from%20iOS%20(5)%20(1).png?raw=true"
-      },
-      "screenshots": [
-          {
-              "normal": "https://github.com/Hub-of-all-Things/exchange-assets/blob/master/Sentiments/Image%20from%20iOS%20(5)%20(1).png?raw=true"
-          },
-          {
-              "normal": "https://github.com/Hub-of-all-Things/exchange-assets/blob/master/Sentiments/Image%20from%20iOS%20(5)%20(1).png?raw=true"
-          }
-      ]
-    }'
-    WHERE id = 'sentiment-tracker';
-
---changeset hubofallthings:changedSentimentFunctionIcon2 context:data
-
-UPDATE hat.she_function
-    SET info.graphics = '
+    SET graphics = '
     {
       "banner": {
           "normal": ""
